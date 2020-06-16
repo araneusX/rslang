77a44ee9f192b38301aa91cc132673f15ce9244e
Anastasia Zizeko <nastjaznf@gmail.com>
2020-06-16 20:18:53 +0300

    feat: add requests functions

---
+++ src/components/requests.ts
@@ -0,0 +1,33 @@
+export const logInUser = async (user: object) => {
+  const url = 'https://afternoon-falls-25894.herokuapp.com/signin';
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  };
+
+  const request = new Request(url, options);
+  const response = await fetch(request);
+  const content = await response.json();
+  console.log(content);
+};
+
+export const createUser = async (user: object) => {
+  const url = 'https://afternoon-falls-25894.herokuapp.com/users';
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  };
+
+  const request = new Request(url, options);
+  const response = await fetch(request);
+  const content = await response.json();
+  console.log(content);
+};
