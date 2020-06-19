f084bce612c9e56a07ee3813375118a95b5f9be8
Olga Bainova <terra.456@yandex.ru>
2020-06-20 02:12:47 +0500

    fix: make settings form

--- src/pages/settings/components/settingsForm.tsx
+++ src/pages/settings/components/settingsForm.tsx
@@ -1,44 +1,55 @@
 import React from 'react';
 
 import style from './settingsForm.module.scss';
-import SettingsFormInput from './SettingsFormInput';
-
-type ChecboxParameters = {name: string, title: string, isChecked: boolean};
-
-const addImage: ChecboxParameters = {
-  name: 'image',
-  title: 'Add image to the card',
-  isChecked: false
-};
 
 const SettingsForm = () => (
   <form className={`${style.settingsForm}`}>
-    <SettingsFormInput state={addImage} />
+    <fieldset>
+      <legend>Quantyty of new words</legend>
+      <input id="cards-quantity" type="number" name="cards-quantity" min="5" max="50" defaultValue="10" />
+      <label htmlFor="cards-quantity">cards quantity for one day</label>
+      <br />
+      <input id="words-quantity" type="number" name="words-quantity" min="5" max="50" defaultValue="10" />
+      <label htmlFor="words-quantity">New words quantity</label>
+      <br />
+    </fieldset>
+    <fieldset>
+      <legend>Cards view</legend>
+      <input id="image" type="checkbox" name="image" />
+      <label htmlFor="image">Add image to the card</label>
+      <br />
+      <input id="pronounse" type="checkbox" name="pronounse" />
+      <label htmlFor="pronounse">Add pronounse to the card</label>
+      <br />
+      <input id="transcription" type="checkbox" name="transcription" />
+      <label htmlFor="transcription">Add transcription to the card</label>
+      <br />
+      <input id="translate" type="checkbox" name="translate" />
+      <label htmlFor="translate">Add translate to the card</label>
+      <br />
+      <input id="example" type="checkbox" name="example" />
+      <label htmlFor="example">Add example to the card</label>
+      <br />
+      <input id="explain" type="checkbox" name="explain" />
+      <label htmlFor="explain">Add explain to the card</label>
+      <br />
+    </fieldset>
+    <fieldset>
+      <legend>Control elements</legend>
+      <input id="show-answer" type="checkbox" name="show-answer" />
+      <label htmlFor="show-answer">Add &quot;show answer&quot; button</label>
+      <br />
+      <input id="delete-button" type="checkbox" name="delete-button" />
+      <label htmlFor="delete-button">Add &quot;delete&quot; button</label>
+      <br />
+      <input id="put-into-hard" type="checkbox" name="put-into-hard" />
+      <label htmlFor="put-into-hard">Add button to put word into hard</label>
+      <br />
+      <input id="grade" type="checkbox" name="grade" />
+      <label htmlFor="grade">Add &quot;grade&quot; buttons</label>
+      <br />
+    </fieldset>
   </form>
 );
 
 export default SettingsForm;
-
-/* <input type="number" name ="cards-quantity">cards quantity for one day</input>
-    <input type="number" name ="words-quantity">new words quantity</input>
-    <fieldset>
-      <input type="checkbox" name="image">Add image to the card</input>
-      <input type="checkbox" name="pronounse">Add pronounse to the card</input>
-      <input type="checkbox" name="transcription">Add transcription to the card</input>
-      <input type="checkbox" name="translate">Add translate to the card</input>
-      <input type="checkbox" name="example">Add example sentence to the card</input>
-      <input type="checkbox" name="explain">Add explain sentence to the card</input>
-    </fieldset>
-    <input type="checkbox" name="show-answer">Add "show answer" button</input>
-    <input type="checkbox" name="delete-button">Add "delete" button</input>
-    <input type="checkbox" name="put-into-hard">Add button to put word into hard</input>
-    <input type="checkbox" name="grade">Add "grade" buttons</input> */
-/*
-Add input for cards quantity for one day
-Add input for new words quantity
-Add input for card settings (image, pronounse, transcription, translate, example sentence, explain sentence)
-Checkbox "Add "show answer" button"
-Checkbox "Add "delete" button"
-Checkbox "Add button to put word into hard"
-Checkbox "Add "grade" buttons"
-*/
