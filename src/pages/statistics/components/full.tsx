83df751a0721f53a044ee37f4e4a1bc782a7335a
PavelChukashev <chukashov93@gmail.com>
2020-06-21 18:05:19 +0300

    fix: eslint fixes

--- src/pages/statistics/components/full.tsx
+++ src/pages/statistics/components/full.tsx
@@ -1,16 +1,13 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import style from './stats.module.scss';
 import { BackendContext } from '../../../backend/backendProvider';
 
-
 const Full = () => {
-    const {getAllDayStatistics} = useContext(BackendContext);
+  const { getAllDayStatistics } = useContext(BackendContext);
 
-    return (
-        <div className={style.graph}>Graph</div>
-    )
-}
-
+  return (
+    <div className={style.graph}>Graph</div>
+  );
+};
 
 export default Full;
-
--- src/pages/statistics/statistics.tsx
+++ src/pages/statistics/statistics.tsx
@@ -1,21 +1,18 @@
-
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Session from './components/session';
-import Full from './components/full'
+import Full from './components/full';
 import style from './statistics.module.scss';
 
-
 const Statistics = () => {
+  const [isToggled, setToggled] = useState(true);
+  const toggleTrueFalse = () => setToggled(!isToggled);
 
-    const [isToggled, setToggled] = useState(true)
-    const toggleTrueFalse = () => setToggled(!isToggled);
-
-    return (
-        <div className={style.statsContainer}>
-        <button className={style.statsButton} onClick={toggleTrueFalse}>Session stats / Total stats</button>
-        { isToggled ? <Session /> : <Full /> }
-        </div>
-    )
-}
+  return (
+    <div className={style.statsContainer}>
+      <button className={style.statsButton} type="button" onClick={toggleTrueFalse}>Session stats / Total stats</button>
+      { isToggled ? <Session /> : <Full /> }
+    </div>
+  );
+};
 
 export default Statistics;
