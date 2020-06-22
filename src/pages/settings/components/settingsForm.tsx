666c0a6858fd6fd878e2b204828af2d4b3030aa1
Alex Leshenko <alex.l@sca.auction>
2020-06-22 14:06:49 +0300

    fix: fix form changeHandler function and local state

--- src/pages/settings/components/settingsForm.tsx
+++ src/pages/settings/components/settingsForm.tsx
@@ -1,100 +1,80 @@
-import React, { useState, useContext } from 'react';
-import { StateContext } from '../../../store/stateProvider';
-import { BackendContext } from '../../../backend/backendProvider';
-import { Settings } from '../../../store/store';
+import React, { useState, useContext } from "react";
+import { StateContext } from "../../../store/stateProvider";
+import { BackendContext } from "../../../backend/backendProvider";
 
-import style from './settingsForm.module.scss';
+import style from "./settingsForm.module.scss";
 
 const SettingsForm = () => {
   const { state, dispatch } = useContext(StateContext);
   const { uploadSettings } = useContext(BackendContext);
   const { settings } = state;
 
-  const { userId } = state.auth;
-  const { authToken } = state.auth;
-
-  const [cardsQuantity, setCardsQuantity] = useState(settings.wordsPerDay);
-  const [wordsQuantity, setWordsQuantity] = useState(settings.optional.maxCountCard);
-  const [imageToCard, setImageToCard] = useState(settings.optional.imageToCard);
-  const [pronounseToCard, setPronounseToCardy] = useState(settings.optional.pronounseToCard);
-  const [transcriptionToCard, setTranscriptionToCard] = useState(settings.optional.transcriptionToCard);
-  const [translateToTheCard, setTranslateToTheCard] = useState(settings.optional.translateToTheCard);
-  const [exampleToCard, setExampleToCard] = useState(settings.optional.exampleToCard);
-  const [explainToCard, setExplainToCard] = useState(settings.optional.explainToCard);
-  const [showAnswerButton, setShowAnswerButton] = useState(settings.optional.showAnswerButton);
-  const [wordDeleteButton, setWordDeleteButton] = useState(settings.optional.wordDeleteButton);
-  const [addToDifficultWordsButton, setAddToDifficultWordsButton] = useState(settings.optional.addToDifficultWordsButton);
-  const [addGrageButton, setAddGrageButton] = useState(settings.optional.addGrageButton);
-
+  const [setting, setSetting] = useState(settings);
   const [loader, setLoader] = useState(false);
 
-  function changeHandler(event: React.ChangeEvent<HTMLInputElement>) : void {
-    const { value } = event.target;
-    const typeOfInput = event.target.name;
-    switch (typeOfInput) {
-      case 'cards-quantity':
-        setCardsQuantity(Number(value));
+  function changeHandler(event: React.ChangeEvent<HTMLInputElement>): void {
+    const { value, name } = event.target;
+    const newSetting = {
+      ...setting,
+    };
+
+    switch (name) {
+      case "cards-quantity":
+        newSetting.wordsPerDay = Number(value);
         break;
-      case 'words-quantity':
-        setWordsQuantity(Number(value));
+      case "words-quantity":
+        setting.optional.maxCountCard = Number(value);
         break;
-      case 'image':
-        setImageToCard(!imageToCard);
+      case "image":
+        setting.optional.imageToCard = !setting.optional.imageToCard;
         break;
-      case 'pronounse':
-        setPronounseToCardy(!pronounseToCard);
+      case "pronounse":
+        setting.optional.pronounseToCard = !setting.optional.pronounseToCard;
         break;
-      case 'transcription':
-        setTranscriptionToCard(!transcriptionToCard);
+      case "transcription":
+        setting.optional.transcriptionToCard = !setting.optional
+          .transcriptionToCard;
         break;
-      case 'translate':
-        setTranslateToTheCard(!translateToTheCard);
+      case "translate":
+        setting.optional.translateToTheCard = !setting.optional
+          .translateToTheCard;
         break;
-      case 'example':
-        setExampleToCard(!exampleToCard);
+      case "example":
+        setting.optional.exampleToCard = !setting.optional.exampleToCard;
         break;
-      case 'explain':
-        setExplainToCard(!explainToCard);
+      case "explain":
+        setting.optional.explainToCard = !setting.optional.explainToCard;
         break;
-      case 'show-answer':
-        setShowAnswerButton(!showAnswerButton);
+      case "show-answer":
+        setting.optional.showAnswerButton = !setting.optional.showAnswerButton;
         break;
-      case 'delete-button':
-        setWordDeleteButton(!wordDeleteButton);
+      case "delete-button":
+        setting.optional.wordDeleteButton = !setting.optional.wordDeleteButton;
         break;
-      case 'put-into-hard':
-        setAddToDifficultWordsButton(!addToDifficultWordsButton);
+      case "put-into-hard":
+        setting.optional.addToDifficultWordsButton = !setting.optional
+          .addToDifficultWordsButton;
         break;
-      case 'grade':
-        setAddGrageButton(!addGrageButton);
+      case "grade":
+        setting.optional.addGrageButton = !setting.optional.addGrageButton;
         break;
       default:
         break;
     }
+    setSetting(newSetting);
   }
 
-  const submitHandler = async (event : React.FormEvent<HTMLFormElement>) => {
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setLoader(true);
-    const newStetting:Settings = {
-      wordsPerDay: cardsQuantity,
-      optional: {
-        maxCountCard: wordsQuantity,
-        imageToCard,
-        pronounseToCard,
-        transcriptionToCard,
-        translateToTheCard,
-        exampleToCard,
-        explainToCard,
-        showAnswerButton,
-        wordDeleteButton,
-        addToDifficultWordsButton,
-        addGrageButton
-      }
-    };
-    const data = await uploadSettings(userId, authToken, newStetting);
+    debugger;
+    const data = await uploadSettings(
+      state.auth.userId,
+      state.auth.authToken,
+      setting
+    );
     if (data) {
-      dispatch({ type: 'SET_SETTING', value: data });
+      dispatch({ type: "SET_SETTING", value: data });
     }
     setLoader(false);
   };
@@ -104,12 +84,28 @@ const SettingsForm = () => {
       <fieldset>
         <legend>Quantyty of new words</legend>
         <label htmlFor="cards-quantity">
-          <input onChange={changeHandler} value={cardsQuantity} id="cards-quantity" type="number" name="cards-quantity" min="5" max="50" />
+          <input
+            onChange={changeHandler}
+            value={setting.wordsPerDay}
+            id="cards-quantity"
+            type="number"
+            name="cards-quantity"
+            min="5"
+            max="50"
+          />
           cards quantity for one day
         </label>
         <br />
         <label htmlFor="words-quantity">
-          <input onChange={changeHandler} value={wordsQuantity} id="words-quantity" type="number" name="words-quantity" min="5" max="50" />
+          <input
+            onChange={changeHandler}
+            value={setting.optional.maxCountCard}
+            id="words-quantity"
+            type="number"
+            name="words-quantity"
+            min="5"
+            max="50"
+          />
           New words quantity
         </label>
         <br />
@@ -117,32 +113,68 @@ const SettingsForm = () => {
       <fieldset>
         <legend>Cards view</legend>
         <label htmlFor="image">
-          <input onChange={changeHandler} checked={imageToCard} id="image" type="checkbox" name="image" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.imageToCard}
+            id="image"
+            type="checkbox"
+            name="image"
+          />
           <span>Add image to the card</span>
         </label>
         <br />
         <label htmlFor="pronounse">
-          <input onChange={changeHandler} checked={pronounseToCard} id="pronounse" type="checkbox" name="pronounse" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.pronounseToCard}
+            id="pronounse"
+            type="checkbox"
+            name="pronounse"
+          />
           <span>Add pronounse to the card</span>
         </label>
         <br />
         <label htmlFor="transcription">
-          <input onChange={changeHandler} checked={transcriptionToCard} id="transcription" type="checkbox" name="transcription" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.transcriptionToCard}
+            id="transcription"
+            type="checkbox"
+            name="transcription"
+          />
           <span>Add transcription to the card</span>
         </label>
         <br />
         <label htmlFor="translate">
-          <input onChange={changeHandler} checked={translateToTheCard} id="translate" type="checkbox" name="translate" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.translateToTheCard}
+            id="translate"
+            type="checkbox"
+            name="translate"
+          />
           <span>Add translate to the card</span>
         </label>
         <br />
         <label htmlFor="example">
-          <input onChange={changeHandler} checked={exampleToCard} id="example" type="checkbox" name="example" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.exampleToCard}
+            id="example"
+            type="checkbox"
+            name="example"
+          />
           <span>Add example to the card</span>
         </label>
         <br />
         <label htmlFor="explain">
-          <input onChange={changeHandler} checked={explainToCard} id="explain" type="checkbox" name="explain" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.explainToCard}
+            id="explain"
+            type="checkbox"
+            name="explain"
+          />
           <span>Add explain to the card</span>
         </label>
         <br />
@@ -150,29 +182,51 @@ const SettingsForm = () => {
       <fieldset>
         <legend>Control elements</legend>
         <label htmlFor="show-answer">
-          <input onChange={changeHandler} checked={showAnswerButton} id="show-answer" type="checkbox" name="show-answer" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.showAnswerButton}
+            id="show-answer"
+            type="checkbox"
+            name="show-answer"
+          />
           <span>Add &quot;show answer&quot; button</span>
         </label>
         <br />
         <label htmlFor="delete-button">
-          <input onChange={changeHandler} checked={wordDeleteButton} id="delete-button" type="checkbox" name="delete-button" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.wordDeleteButton}
+            id="delete-button"
+            type="checkbox"
+            name="delete-button"
+          />
           <span>Add &quot;delete&quot; button</span>
         </label>
         <br />
         <label htmlFor="put-into-hard">
-          <input onChange={changeHandler} checked={addToDifficultWordsButton} id="put-into-hard" type="checkbox" name="put-into-hard" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.addToDifficultWordsButton}
+            id="put-into-hard"
+            type="checkbox"
+            name="put-into-hard"
+          />
           <span>Add button to put word into hard</span>
         </label>
         <br />
         <label htmlFor="grade">
-          <input onChange={changeHandler} checked={addGrageButton} id="grade" type="checkbox" name="grade" />
+          <input
+            onChange={changeHandler}
+            checked={setting.optional.addGrageButton}
+            id="grade"
+            type="checkbox"
+            name="grade"
+          />
           <span>Add &quot;grade&quot; buttons</span>
         </label>
         <br />
       </fieldset>
-      {loader
-        ? 'Loading...'
-        : <input type="submit" value="Save" />}
+      {loader ? "Loading..." : <input type="submit" value="Save" />}
     </form>
   );
 };
