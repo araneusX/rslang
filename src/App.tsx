65f89f18af111d837cce2a83188db8f9a0ad36ea
Alexander Oleshkevich <araneusx@gmail.com>
2020-06-23 14:24:24 +0300

    feat: improve routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   BrowserRouter, Switch, Route, Redirect
 } from 'react-router-dom';
-import { StateProvider } from './store/stateProvider';
-import { StatisticsProvider } from './statistics/statisticsProvider';
-
 import {
   Authorization, Main, Settings, Statistics
 } from './pages';
 
 import { Header } from './commonComponents';
+import { StateContext } from './store/stateProvider';
 
 function App() {
+  const { state } = useContext(StateContext);
+  const { isAuth } = state.auth;
   return (
     <>
-      <StateProvider>
-        <StatisticsProvider>
-          <BrowserRouter>
-            <Header />
-            <Switch>
-              <Route exact path="/authorization" component={Authorization} />
-              <Route exact path="/main" component={Main} />
-              <Route exact path="/settings" component={Settings} />
-              <Route exact path="/statistics" component={Statistics} />
-              <Redirect to="/main" />
-            </Switch>
-          </BrowserRouter>
-        </StatisticsProvider>
-      </StateProvider>
+      <BrowserRouter>
+        {isAuth && <Header />}
+        <Switch>
+          <Route exact path="/authorization" component={Authorization} />
+          {isAuth && (
+          <>
+            <Route exact path="/main" component={Main} />
+            <Route exact path="/settings" component={Settings} />
+            <Route exact path="/statistics" component={Statistics} />
+          </>
+          )}
+          <Redirect to={isAuth ? '/main' : '/authorization'} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
