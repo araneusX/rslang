42fca736c5972841ad67c4c64c949af8fb1e88c1
keksik77 <lukashevich7tim@gmail.com>
2020-06-30 03:18:24 +0300

    feat: add automatic transition to the next card

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,23 +1,32 @@
 import React, { useContext, useState, useEffect } from 'react';
 import style from './main.module.scss';
 import Card from './components/card';
-import { BackendWordInterface } from '../../types';
+import { BackendWordInterface, StatisticsInterface } from '../../types';
 import cardObj from '../../mosk/testCardObj';
 import { StateContext } from '../../store/stateProvider';
 import trainGameCard from './components/training';
+import { StatisticsContext } from '../../statistics/statisticsProvider';
 
 const Main = () => {
   const { state } = useContext(StateContext);
+  const statistics = useContext(StatisticsContext) as StatisticsInterface;
   const [cardObject, setCardObject] = useState<BackendWordInterface >(cardObj[0] as BackendWordInterface);
   const [startPreview, setStart] = useState(true);
-  const [count, setCount] = useState(0);
+  const [endPreview, setEndPreview] = useState(false);
+
+  const [count, setCount] = useState<number>(statistics.getDayStatistics().cards);
   const [answer, setAns] = useState(false);
+  const [soundState, setSound] = useState(true);
 
   useEffect(() => {
     let ignore = false;
     async function fetchData() {
+      if (count < state.settings.wordsPerDay) {
       const result = await (trainGameCard(state.auth.userId, state.auth.token));
-      if (!ignore) setCardObject(result); console.log(result);
+      if (!ignore) console.log('текущая карточка', result); setCardObject(result);
+      } else {
+        setEndPreview(true);
+      }
     }
     fetchData();
     return () => { ignore = true; };
@@ -33,7 +42,7 @@ const Main = () => {
     showAnswerButton,
     transcriptionToCard,
     translateToTheCard,
-    wordDeleteButton
+    wordDeleteButton,
   } = state.settings.optional;
 
   const settings = {
@@ -46,7 +55,7 @@ const Main = () => {
     showAnswerButton,
     transcriptionToCard,
     translateToTheCard,
-    wordDeleteButton
+    wordDeleteButton,
   };
 
   const handleNext = () => {
@@ -58,36 +67,73 @@ const Main = () => {
     }
   };
 
+  const handleSoundControl = () => {
+    if (soundState) {
+      setSound(false);
+    } else {
+      setSound(true);
+    }
+  }
+
+  const nextCard = () => {
+    setAns(false);
+    setCount(count + 1);
+  }
+
   return (
     <>
       <div className={style.learnContainer}>
-        {startPreview ? (
+        {startPreview || endPreview ? (
           <div className={style.startBasicGameContainer}>
-            <h2>Тренировка</h2>
-            <p>
+            {endPreview ? (
+              <>
+                <h2>На сегодня все...</h2>
+                <p>
+                &#8195;Ты отлично постарался! Возвращайся завтра за новыми знаниями!
+                </p>
+              </>
+            ) : (
+              <>
+              <h2>Тренировка</h2>
+              <p>
               &#8195;Улучшай свои знания с помощью крутых и не скучных тренировок.
               По мере изучения языка у вас будут накапливаться слова на повторение.
               Так что не удивляйтесь, если слова будут иногда повторяться.
-            </p>
-            <button
-              className={style.startLearnButton}
-              onClick={() => { setStart(false); }}
-            >
-              Начать изучать
-            </button>
+              </p>
+              <button
+                className={style.startLearnButton}
+                onClick={() => { setStart(false); }}
+              >
+                Начать изучать
+              </button>
+              </>
+            )}
           </div>
         ) : (
           <>
             <div className={style.sliderContainer}>
-              <Card cardObj={cardObject} settings={settings} answer={answer} callback={setAns} count={count} />
+              <Card
+                cardObj={cardObject}
+                settings={settings}
+                answer={answer}
+                callback={setAns}
+                count={count}
+                soundState={soundState}
+                nextCard={nextCard}
+              />
             </div>
             <div className={style.controlContainer}>
+              { soundState ? (
+                <button className={style.soundOn} onClick={handleSoundControl}>Выключить звук</button>
+              ) : (
+                <button className={style.soundOff} onClick={handleSoundControl}>Включить звук</button>
+              )}
               <button onClick={() => { setAns(true); }}>Ответить</button>
               <button onClick={handleNext}>Далее</button>
               <div className={style.progressBar}>
-                {count + 1}
-                <progress value={count + 1} max={20} />
-                {20}
+                {count}
+                <progress value={count} max={state.settings.wordsPerDay} />
+                {state.settings.wordsPerDay}
               </div>
             </div>
           </>
