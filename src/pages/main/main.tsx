906138153958a0917a30e582d0257569fc197135
Anastasia Zizeko <nastjaznf@gmail.com>
2020-07-07 11:44:49 +0300

    feat: add imitation of preloader

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 import style from './main.module.scss';
 import Card from './components/card';
 import cardObj from '../../mosk/testCardObj';
+import Preloader from '../../commonComponents/preloader/Preloader';
 import { StateContext } from '../../store/stateProvider';
 
 const Main = () => {
-  const { state } = useContext(StateContext);
+  const { state, dispatch } = useContext(StateContext);
   const {
     imageToCard,
     pronounseToCard,
@@ -17,6 +18,8 @@ const Main = () => {
     explainToCard
   } = state.settings.optional;
 
+  const { isLoading } = state.preloader;
+
   const settings = {
     imageToCard,
     pronounseToCard,
@@ -25,9 +28,16 @@ const Main = () => {
     exampleToCard,
     explainToCard
   };
+  /*  dispatch({ type: 'SET_LOADING', value: true }); */
+
+  const dispatchLoading = () => {
+    dispatch({ type: 'SET_LOADING', value: false });
+  };
+  setInterval(dispatchLoading, 1500);
 
   return (
     <>
+      {isLoading ? <Preloader /> : null}
       <nav>
         <Link to="/authorization"> Authorization Page </Link>
         <Link to="/main"> Main Page </Link>
