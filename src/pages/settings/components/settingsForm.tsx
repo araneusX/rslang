8c3ce2834e0b0858600f8bbd73157e0fd1029604
mrinex <mrinex@mail.ru>
2020-07-08 17:43:11 +0300

    feat: add style to settings page

--- src/pages/settings/components/settingsForm.tsx
+++ src/pages/settings/components/settingsForm.tsx
@@ -105,10 +105,15 @@ const SettingsForm = () => {
 
   return (
     <form className={`${style.settingsForm}`} onSubmit={submitHandler}>
-      <fieldset>
-        <legend>Quantyty of new words</legend>
-        <label htmlFor="cards-quantity">
+      <fieldset className={style['learning-border']}>
+        <legend className={style['learning-title']}>
+          Настройки
+          <br />
+          обучения
+        </legend>
+        <label className={style['learning-label']} htmlFor="cards-quantity">
           <input
+            className={style['learning-input']}
             onChange={changeHandler}
             value={setting.wordsPerDay}
             id="cards-quantity"
@@ -117,11 +122,11 @@ const SettingsForm = () => {
             min="5"
             max="50"
           />
-          cards quantity for one day
+          <p className={style['learning-day']}>Количество карточек в день:</p>
         </label>
-        <br />
-        <label htmlFor="words-quantity">
+        <label className={style['learning-label']} htmlFor="words-quantity">
           <input
+            className={style['learning-input']}
             onChange={changeHandler}
             value={setting.optional.maxCountCard}
             id="words-quantity"
@@ -130,132 +135,127 @@ const SettingsForm = () => {
             min="5"
             max="50"
           />
-          New words quantity
+          <p className={style['learning-day']}>Количество новых слов в день:</p>
         </label>
-        <br />
-        <label htmlFor="level">
-          Level:
-          <select name="level" id="level" value={setting.optional.level} onChange={changeLevel}>
+        <label className={style['learning-label']} htmlFor="level">
+          <select className={style['learning-level']} name="level" id="level" value={setting.optional.level} onChange={changeLevel}>
             {levelsArray.map((level) => <option key={level} value={level}>{level + 1}</option>)}
           </select>
+          <p className={style['learning-day']}>Уровень:</p>
         </label>
-        <br />
       </fieldset>
-      <fieldset>
-        <legend>Cards view</legend>
-        <label htmlFor="image">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.imageToCard}
-            id="image"
-            type="checkbox"
-            name="image"
-          />
-          <span>Add image to the card</span>
-        </label>
-        <br />
-        <label htmlFor="pronounse">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.pronounseToCard}
-            id="pronounse"
-            type="checkbox"
-            name="pronounse"
-          />
-          <span>Add pronounse to the card</span>
-        </label>
-        <br />
-        <label htmlFor="transcription">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.transcriptionToCard}
-            id="transcription"
-            type="checkbox"
-            name="transcription"
-          />
-          <span>Add transcription to the card</span>
-        </label>
-        <br />
-        <label htmlFor="translate">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.translateToTheCard}
-            id="translate"
-            type="checkbox"
-            name="translate"
-          />
-          <span>Add translate to the card</span>
-        </label>
-        <br />
-        <label htmlFor="example">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.exampleToCard}
-            id="example"
-            type="checkbox"
-            name="example"
-          />
-          <span>Add example to the card</span>
-        </label>
-        <br />
-        <label htmlFor="explain">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.explainToCard}
-            id="explain"
-            type="checkbox"
-            name="explain"
-          />
-          <span>Add explain to the card</span>
-        </label>
-        <br />
+      <fieldset className={style['card-border']}>
+        <legend className={style['learning-title']}>Настройки карточки</legend>
+        <p className={style['card-day']}>На карточке отображать:</p>
+        <div className={style['wrapper-label']}>
+          <label htmlFor="image">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.imageToCard}
+              id="image"
+              type="checkbox"
+              name="image"
+            />
+            <span>Картинка с ассоциацией</span>
+          </label>
+          <label htmlFor="pronounse">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.pronounseToCard}
+              id="pronounse"
+              type="checkbox"
+              name="pronounse"
+            />
+            <span>Автовоспроизведение слова</span>
+          </label>
+          <label htmlFor="transcription">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.transcriptionToCard}
+              id="transcription"
+              type="checkbox"
+              name="transcription"
+            />
+            <span>Транскрипция</span>
+          </label>
+          <label htmlFor="translate">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.translateToTheCard}
+              id="translate"
+              type="checkbox"
+              name="translate"
+            />
+            <span>Перевод слова</span>
+          </label>
+          <label htmlFor="example">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.exampleToCard}
+              id="example"
+              type="checkbox"
+              name="example"
+            />
+            <span>Предложение с примером применения слова</span>
+          </label>
+          <label htmlFor="explain">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.explainToCard}
+              id="explain"
+              type="checkbox"
+              name="explain"
+            />
+            <span>Предложение с объяснением слова</span>
+          </label>
+        </div>
       </fieldset>
-      <fieldset>
-        <legend>Control elements</legend>
-        <label htmlFor="show-answer">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.showAnswerButton}
-            id="show-answer"
-            type="checkbox"
-            name="show-answer"
-          />
-          <span>Add &quot;show answer&quot; button</span>
-        </label>
-        <br />
-        <label htmlFor="delete-button">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.wordDeleteButton}
-            id="delete-button"
-            type="checkbox"
-            name="delete-button"
-          />
-          <span>Add &quot;delete&quot; button</span>
-        </label>
-        <br />
-        <label htmlFor="put-into-hard">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.addToDifficultWordsButton}
-            id="put-into-hard"
-            type="checkbox"
-            name="put-into-hard"
-          />
-          <span>Add button to put word into hard</span>
-        </label>
-        <br />
-        <label htmlFor="grade">
-          <input
-            onChange={changeHandler}
-            checked={setting.optional.addGrageButton}
-            id="grade"
-            type="checkbox"
-            name="grade"
-          />
-          <span>Add &quot;grade&quot; buttons</span>
-        </label>
-        <br />
+      <fieldset className={style['control-border']}>
+        <legend className={style['learning-title']}>Дополнительные настройки</legend>
+        <p className={style['card-day']}>Включить:</p>
+        <div className={style['wrapper-control']}>
+          <label htmlFor="show-answer">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.showAnswerButton}
+              id="show-answer"
+              type="checkbox"
+              name="show-answer"
+            />
+            <span>Просмотр правильного ответа</span>
+          </label>
+          <label htmlFor="delete-button">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.wordDeleteButton}
+              id="delete-button"
+              type="checkbox"
+              name="delete-button"
+            />
+            <span>Удаление хорошо изученные слова из колоды слова</span>
+          </label>
+          <label htmlFor="put-into-hard">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.addToDifficultWordsButton}
+              id="put-into-hard"
+              type="checkbox"
+              name="put-into-hard"
+            />
+            <span>Добавление слова в раздел словаря “сложное”</span>
+          </label>
+          <label htmlFor="grade">
+            <input
+              onChange={changeHandler}
+              checked={setting.optional.addGrageButton}
+              id="grade"
+              type="checkbox"
+              name="grade"
+            />
+            <span>Оценка сложности слова</span>
+          </label>
+        </div>
+        <p className={style.recommendation}>Рекомендация: для более эффективного обучения, включите оценку сложности слова.</p>
       </fieldset>
       {loader ? 'Loading...' : <input type="submit" value="Save" />}
     </form>
