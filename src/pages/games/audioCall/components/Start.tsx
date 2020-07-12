1a1db2b6ba628073e1ff9ad6c068ee702afdd1ac
Alex L <sanekleshenko@yandex.ru>
2020-07-12 18:03:01 +0300

    feat: add translate

--- src/pages/games/audioCall/components/Start.tsx
+++ src/pages/games/audioCall/components/Start.tsx
@@ -22,10 +22,10 @@ const Start = () => {
             <div className={style.startPageWrapper}>
               <div className={style.startCard}>
                 <h2>
-                  Audio Challenge
+                  Аудиовызов
                 </h2>
                 <p>
-                  Audio challenge training develops your listening skills. The more words you know, the more experience you get.
+                  Аудио-тренинг развивает ваши навыки слушания. Чем больше слов вы знаете, тем больше опыта вы получаете.
                 </p>
                 <button type="button" onClick={startGameF}>
                   Start
