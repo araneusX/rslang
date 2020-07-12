86524334f5127c711a1be4aa20a76652bba6649f
Alexander Oleshkevich <araneusx@gmail.com>
2020-07-12 19:36:02 +0300

    fix: fix links

--- src/pages/games/games.tsx
+++ src/pages/games/games.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './games.module.scss';
 
-const Games = () => {
-
-  return (
-    <>
-      <nav className={style.tiles}>
-        <Link to="/games/speakit">
-          <div className={style.tile}>Speak It</div>
-        </Link>
-        <Link to="/games/sprint">
-          <div className={style.tile}>Sprint</div>
-        </Link>
-        <Link to="/games/audio-call">
-          <div className={style.tile}>Audio Call</div>
-        </Link>
-      </nav>
-    </>
-  );
-};
+const Games = () => (
+  <>
+    <nav className={style.tiles}>
+      <Link to="/games/speakit">
+        <div className={style.tile}>Speak It</div>
+      </Link>
+      <Link to="/games/sprint">
+        <div className={style.tile}>Спринт</div>
+      </Link>
+      <Link to="/games/savannah">
+        <div className={style.tile}>Саванна</div>
+      </Link>
+      <Link to="/games/audio-call">
+        <div className={style.tile}>Аудиовызов</div>
+      </Link>
+      <Link to="/games/puzzle">
+        <div className={style.tile}>Пазл</div>
+      </Link>
+    </nav>
+  </>
+);
 export default Games;
