b24cb659129f2c84c59e8e8556234dc3fb6acff1
Alexander Oleshkevich <araneusx@gmail.com>
2020-07-12 22:30:43 +0300

    feat: add base layout

--- src/pages/games/games.tsx
+++ src/pages/games/games.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './games.module.scss';
 
+const games = [
+  ['Ассоциации', '/games/speakit', 'association.jpg'],
+  ['Спринт', '/games/speakit', 'sprint.png'],
+  ['Саванна', '/games/speakit', 'savannah.png'],
+  ['Аудиовызов', '/games/speakit', 'audioCall.png'],
+  ['Puzzle', '/games/speakit', 'puzzle.png'],
+  ['Speak It', '/games/speakit', 'speakit.png']
+];
+
 const Games = () => (
-  <>
+  <div className={style.wrapper}>
     <nav className={style.tiles}>
-      <Link to="/games/speakit">
-        <div className={style.tile}>Speak It</div>
-      </Link>
-      <Link to="/games/sprint">
-        <div className={style.tile}>Спринт</div>
-      </Link>
-      <Link to="/games/savannah">
-        <div className={style.tile}>Саванна</div>
-      </Link>
-      <Link to="/games/audio-call">
-        <div className={style.tile}>Аудиовызов</div>
-      </Link>
-      <Link to="/games/puzzle">
-        <div className={style.tile}>Пазл</div>
-      </Link>
+      {games.map((game) => (
+        <Link to={game[1]} key={game[0]}>
+          <div
+            style={{ background: `${process.env.PUBLIC_URL}/images/tiles/${game[2]}` }}
+            className={style.tile}
+          >
+            {game[0]}
+          </div>
+        </Link>
+      ))}
     </nav>
-  </>
+  </div>
 );
 export default Games;
