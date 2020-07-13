221952a909e7a0f25e8d21c25d6026c4cb74577c
keksik77 <lukashevich7tim@gmail.com>
2020-07-13 09:21:17 +0300

    feat: add promo page

--- src/pages/authorization/authorization.tsx
+++ src/pages/authorization/authorization.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Form from './components/form';
 
 import style from './authorization.module.scss';
+import { Link, Redirect, Route } from 'react-router-dom';
+import { Promo } from '..';
 
 const Authorization = () => (
   <div>
